50336498892347d3562bbe3ec459c1af0838cae8
talus <taluskent@gmail.com>
2026-01-23 12:04:56 +0000

    did the

--- ecm1410-workshop-2-main/HelloWorldApp.java
+++ ecm1410-workshop-2-main/HelloWorldApp.java
@@ -1,3 +1,10 @@
-// Add your code below
-
-
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.print("Hello World! My name is ");
+        System.out.print(args[0]);
+        System.out.println(".");
+        System.out.print("you are age ");
+        System.out.print(args[1]);
+        System.out.println(".");
+    }
+}
