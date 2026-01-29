d6bd6d4634f61f67d1e581552449be788089e1f7
talus <taluskent@gmail.com>
2026-01-29 11:46:06 +0000

    testing if github likes me 8

--- ecm1410-workshop-2-main/HelloWorldApp.java
+++ ecm1410-workshop-2-main/HelloWorldApp.java
@@ -1,6 +1,6 @@
 public class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.print("Hello World! My name is no");
+        System.out.print("Hello World! My name is aaa");
         System.out.print(args[0]);
         System.out.println(".");
         System.out.print("you are aged ");
