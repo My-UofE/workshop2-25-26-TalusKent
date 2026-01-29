0455caa9dc1041c261a327a01810ac7ca765d9b3
talus <tjk219@exeter.ac.uk>
2026-01-29 17:48:32 +0000

    Completed Task 1

--- ecm1410-workshop-2-main/HelloWorldApp.java
+++ ecm1410-workshop-2-main/HelloWorldApp.java
@@ -1,10 +1,10 @@
 public class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.print("ugh");
+        System.out.print("Hello World! My name is ");
         System.out.print(args[0]);
         System.out.println(".");
-        System.out.print("you are aged ");
+        System.out.print(" I am ");
         System.out.print(args[1]);
-        System.out.println(".");
+        System.out.println(" years old.");
     }
 }
