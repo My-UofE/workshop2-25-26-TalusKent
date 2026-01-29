43f77ca53ced36b4ebad4a035b9f7c471b0c5191
talus <tjk219@exeter.ac.uk>
2026-01-29 17:36:11 +0000

    github please i am begginggg againnn pleasee i beggg

--- ecm1410-workshop-2-main/HelloWorldApp.java
+++ ecm1410-workshop-2-main/HelloWorldApp.java
@@ -1,6 +1,6 @@
 public class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.print("suffering 3");
+        System.out.print("ugh");
         System.out.print(args[0]);
         System.out.println(".");
         System.out.print("you are aged ");
